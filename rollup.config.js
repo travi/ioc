177d5cfc6d330a524cf2384b0b7dae3bd58aa079
Matt Travi <programmer@travi.org>
2016-11-18 10:21:50 -0600

    build(rollup): configured rollup to bundle es5 and es6 versions

---
+++ rollup.config.js
@@ -0,0 +1,16 @@
+import babel from 'rollup-plugin-babel';
+
+export default {
+    entry: 'src/container.js',
+    plugins: [
+        babel({
+            babelrc: false,
+            exclude: ['./node_modules/**'],
+            presets: ['es2015-rollup']
+        })
+    ],
+    targets: [
+        {dest: 'lib/ioc.cjs.js', format: 'cjs'},
+        {dest: 'lib/ioc.es.js', format: 'es'}
+    ]
+};
