cf280ab3a46d20d7aca4c0f6d176d5eb7c0ee17e
Matt Travi <developer@travi.org>
2018-07-18 11:14:53 -0500

    feat(unregistered): threw an error to give fast feedback when using before registering

--- src/container.js
+++ src/container.js
@@ -5,5 +5,7 @@ export function register(name, dependency) {
 }
 
 export function use(name) {
+  if (!dependencies[name]) throw new Error('Attempted to use a dependency that has not been registered');
+
   return dependencies[name];
 }
--- test/unit/container-test.js
+++ test/unit/container-test.js
@@ -3,12 +3,16 @@ import any from '@travi/any';
 import {register, use} from '../../src/container';
 
 suite('ioc container', () => {
-  it('should store dependency instances', () => {
+  test('that dependencies are stored and are retrievable by name', () => {
     const dependency = any.simpleObject();
     const name = any.string();
 
     register(name, dependency);
 
     assert.equal(use(name), dependency);
+  });
+
+  test('that an error is throw when attemting to retrieve a dependency that has not been registered', () => {
+    assert.throws(() => use(any.word()), 'Attempted to use a dependency that has not been registered');
   });
 });
